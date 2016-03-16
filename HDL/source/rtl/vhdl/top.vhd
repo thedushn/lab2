e894c55667d2e3496f85e4262bd7b60e8dd944ea
Dusan Mitrovic <thedushn@email.com>
2016-03-16 13:56:13 +0100

    Napravio sam dir_red gren i blue

--- HDL/source/rtl/vhdl/top.vhd
+++ HDL/source/rtl/vhdl/top.vhd
@@ -250,6 +250,24 @@ begin
   --dir_red
   --dir_green
   --dir_blue
+dir_red <=  x"ff" when dir_pixel_column >= 0 and dir_pixel_column < H_RES/8 else
+					x"ff" when dir_pixel_column >= H_RES/8 and dir_pixel_column < 2*H_RES/8 else
+					x"ff" when dir_pixel_column >= 4*H_RES/8 and dir_pixel_column < 5*H_RES/8 else
+					x"ff" when dir_pixel_column >= 5*H_RES/8 and dir_pixel_column < 6*H_RES/8 else
+					x"00";
+					
+					
+dir_green <= x"ff" when dir_pixel_column >= 0 and dir_pixel_column < H_RES/8 else
+				 x"ff" when dir_pixel_column >= H_RES/8 and dir_pixel_column < 2*H_RES/8 else
+				 x"ff" when dir_pixel_column >= 2*H_RES/8 and dir_pixel_column < 3*H_RES/8 else
+				  x"ff" when dir_pixel_column >= 3*H_RES/8 and dir_pixel_column < 4*H_RES/8 else
+				  x"00";
+				  
+dir_blue <= x"ff" when dir_pixel_column >= 0 and dir_pixel_column < H_RES/8 else
+					x"ff" when dir_pixel_column >= 2*H_RES/8 and dir_pixel_column < 3*H_RES/8 else
+					x"ff" when dir_pixel_column >= 4*H_RES/8 and dir_pixel_column < 5*H_RES/8 else
+					x"ff" when dir_pixel_column >= 6*H_RES/8 and dir_pixel_column < 7*H_RES/8 else
+					x"00";	
  
   -- koristeci signale realizovati logiku koja pise po TXT_MEM
   --char_address
